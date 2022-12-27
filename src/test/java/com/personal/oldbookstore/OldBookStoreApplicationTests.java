795bf8bf59d7b426130a7205d5a9b988339b2fad
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-12-27 16:58:36 +0900

    project initial setup

---
+++ src/test/java/com/personal/oldbookstore/OldBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.oldbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
