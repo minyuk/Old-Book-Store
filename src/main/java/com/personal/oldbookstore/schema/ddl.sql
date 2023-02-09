03d1fa6d3ca0b89af8588f1e5e125ed068c25e35
Minhyeok Kang <kmhyeok0816@gmail.com>
2023-02-09 14:57:52 +0900

    chore : db table init

--- src/main/java/com/personal/oldbookstore/schema/ddl.sql
+++ src/main/java/com/personal/oldbookstore/schema/ddl.sql
@@ -1,100 +1,101 @@
 create table Basket (
-   basket_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    count integer not null,
-    item_id bigint,
-    user_id bigint,
-    primary key (basket_id)
-) engine=InnoDB
+       basket_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        count integer not null,
+        item_id bigint,
+        user_id bigint,
+        primary key (basket_id)
+    ) engine=InnoDB
 
 create table Comment (
-   comment_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    contents varchar(255) not null,
-    depth integer,
-    parentId bigint,
-    viewStatus varchar(255) not null,
-    item_id bigint,
-    user_id bigint,
-    primary key (comment_id)
-) engine=InnoDB
+       comment_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        contents varchar(255) not null,
+        depth integer,
+        parentId bigint,
+        viewStatus varchar(255) not null,
+        item_id bigint,
+        user_id bigint,
+        primary key (comment_id)
+    ) engine=InnoDB
 
 create table Item (
-   item_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    bookAuthor varchar(255) not null,
-    bookTitle varchar(255) not null,
-    category varchar(255) not null,
-    contents TEXT not null,
-    likeCount bigint,
-    name varchar(255) not null,
-    price integer not null,
-    saleStatus varchar(255),
-    stock integer not null,
-    viewCount bigint,
-    user_id bigint,
-    primary key (item_id)
-) engine=InnoDB
+       item_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        bookAuthor varchar(255) not null,
+        bookTitle varchar(255) not null,
+        category varchar(255) not null,
+        commentCount bigint,
+        contents TEXT not null,
+        likeCount bigint,
+        name varchar(255) not null,
+        price integer not null,
+        saleStatus varchar(255),
+        stock integer not null,
+        viewCount bigint,
+        user_id bigint,
+        primary key (item_id)
+    ) engine=InnoDB
 
 create table ItemFile (
-   item_file_id bigint not null auto_increment,
-    fileName varchar(255) not null,
-    filePath varchar(255) not null,
-    item_id bigint,
-    primary key (item_file_id)
-) engine=InnoDB
+       item_file_id bigint not null auto_increment,
+        fileName varchar(255) not null,
+        filePath varchar(255) not null,
+        item_id bigint,
+        primary key (item_file_id)
+    ) engine=InnoDB
 
 create table LikeItem (
-   like_item_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    item_id bigint,
-    user_id bigint,
-    primary key (like_item_id)
-) engine=InnoDB
+       like_item_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        item_id bigint,
+        user_id bigint,
+        primary key (like_item_id)
+    ) engine=InnoDB
 
 create table OrderItem (
-   order_item_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    count integer not null,
-    orderPrice integer not null,
-    item_id bigint,
-    order_id bigint,
-    primary key (order_item_id)
-) engine=InnoDB
+       order_item_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        count integer not null,
+        orderPrice integer not null,
+        item_id bigint,
+        order_id bigint,
+        primary key (order_item_id)
+    ) engine=InnoDB
 
 create table Orders (
-   order_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    defaultAddress varchar(255),
-    detailAddress varchar(255),
-    extraAddress varchar(255),
-    postcode varchar(255),
-    orderDate datetime(6) not null,
-    orderStatus varchar(255),
-    payment varchar(255) not null,
-    phone varchar(11) not null,
-    recipient varchar(255) not null,
-    user_id bigint,
-    primary key (order_id)
-) engine=InnoDB
+       order_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        defaultAddress varchar(255),
+        detailAddress varchar(255),
+        extraAddress varchar(255),
+        postcode varchar(255),
+        orderDate datetime(6) not null,
+        orderStatus varchar(255),
+        payment varchar(255) not null,
+        phone varchar(11) not null,
+        recipient varchar(255) not null,
+        user_id bigint,
+        primary key (order_id)
+    ) engine=InnoDB
 
 create table User (
-   user_id bigint not null auto_increment,
-    createdDate datetime(6),
-    modifiedDate datetime(6),
-    email varchar(255) not null,
-    nickname varchar(255) not null,
-    password varchar(255) not null,
-    provider varchar(255),
-    role varchar(255) not null,
-    primary key (user_id)
-) engine=InnoDB
+       user_id bigint not null auto_increment,
+        createdDate datetime(6),
+        modifiedDate datetime(6),
+        email varchar(255) not null,
+        nickname varchar(255) not null,
+        password varchar(255) not null,
+        provider varchar(255),
+        role varchar(255) not null,
+        primary key (user_id)
+    ) engine=InnoDB
 
 CREATE TABLE SPRING_SESSION (
     PRIMARY_ID CHAR(36) NOT NULL,
